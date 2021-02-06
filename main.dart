92df2b5834a0d4141d495ac012f6e8350f537aae
WeiJie22 <74247436+WeiJie22@users.noreply.github.com>
2021-02-06 16:24:11 +0800

    added some colors

--- main.dart
+++ main.dart
@@ -25,101 +25,155 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  var _backgroundColor = Colors.white;
-  int _pageState = 0;
-
   @override
   Widget build(BuildContext context) {
-    switch (_pageState) {
-      case 0:
-        _backgroundColor = Colors.white;
-        break;
-      case 1:
-        _backgroundColor = Colors.green[400];
-        break;
-    }
-
     return Scaffold(
-      body: SafeArea(
-        child: AnimatedContainer(
-          curve: Curves.fastLinearToSlowEaseIn,
-          duration: Duration(milliseconds: 300),
-          child: Center(
-            child: Column(
-              children: <Widget>[
-                Container(
-                  child: Text(
-                    "Login",
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-                  ),
+      body: Container(
+        color: Colors.white,
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              Container(
+                child: Text(
+                  "Login",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
                 ),
-                Container(
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
-                    child: Column(
-                      children: <Widget>[
-                        TextField(
-                          obscureText: false,
-                          decoration: InputDecoration(
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(20),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Container(
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
+                  child: Column(
+                    children: <Widget>[
+                      TextField(
+                        obscureText: false,
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            prefixIcon: Icon(Icons.email),
+                            labelText: 'Email'),
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            prefixIcon: Icon(Icons.lock),
+                            labelText: 'Password'),
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          TextButton(
+                              onPressed: () {},
+                              child: Text(
+                                "Forget Password",
+                                style: TextStyle(color: Colors.green[300]),
+                              )),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      SizedBox(
+                        height: 40,
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors.green,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(30)),
+                          ),
+                          onPressed: () {},
+                          child: Text(
+                            'Login',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      SizedBox(
+                        height: 40,
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors.green,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(30)),
+                          ),
+                          onPressed: () {},
+                          child: Text(
+                            'Create new account',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Container(
+                        child: Text(
+                          ' ______________ or login in with ______________',
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                      ),
+
+                      SizedBox(height: 15,),
+
+                      Container(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            GestureDetector(
+                              onTap: (){},
+                              child: CircleAvatar(
+                                radius: 20,
+                                backgroundImage: AssetImage('assets/facebookicon.png'),
                               ),
-                              prefixIcon: Icon(Icons.people),
-                              labelText: 'Username'),
-                        ),
-                        SizedBox(
-                          height: 30,
-                        ),
-                        TextField(
-                          obscureText: true,
-                          decoration: InputDecoration(
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(20),
+                            ),
+
+                            SizedBox(width: 30,),
+
+                            GestureDetector(
+                              onTap: (){},
+                              child: CircleAvatar(
+                                radius: 20,
+                                backgroundImage: AssetImage('assets/googleicon.jpg'),
+                                foregroundColor: Colors.grey,
                               ),
-                              prefixIcon: Icon(Icons.lock),
-                              labelText: 'Password'),
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            TextButton(
-                                onPressed: () {},
-                                child: Text("Forget Password")),
+                            )
                           ],
                         ),
-                        SizedBox(
-                          height: 30,
-                        ),
-                        SizedBox(
-                          height: 40,
-                          width: double.infinity,
-                          child: ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(30)),
-                            ),
-                            onPressed: () {},
-                            child: Text(
-                              'Login',
-                              style: TextStyle(fontSize: 20),
-                            ),
-                          ),
-                        )
-                      ],
-                    ),
+                      )
+                    ],
                   ),
                 ),
-                Container(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[],
-                  ),
-                )
-              ],
-            ),
+              ),
+              Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[],
+                ),
+              )
+            ],
           ),
         ),
       ),
@@ -133,9 +187,18 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  var _backgroundColor = Colors.white;
+  var _backgroundColor = [
+    Colors.green[200],
+    Colors.green[300],
+    Colors.green[500]];
   var _headingColor = Colors.black;
   double _loginYOffSet = 0;
+
+  double _titleXOffSet = 0;
+  double _titleYOffSet = 0;
+
+  double _iconXOffSet = 0;
+  double _iconYOffSet = 0;
 
   double _windowWidth = 0;
   double _windowHeight = 0;
@@ -143,97 +206,132 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-
     _windowHeight = MediaQuery.of(context).size.height;
     _windowWidth = MediaQuery.of(context).size.width;
-
 
     switch (_pageState) {
       case 0:
         _loginYOffSet = _windowHeight;
-        _backgroundColor = Colors.white;
+        _titleXOffSet = 0;
+        _titleYOffSet = 0;
+        _iconXOffSet = 0;
+        _iconYOffSet = 0;
+        _backgroundColor = [
+          Colors.green[200],
+          Colors.green[300],
+          Colors.green[500]];
         _headingColor = Colors.black;
         break;
       case 1:
-        _backgroundColor = Colors.green[400];
+        _backgroundColor = [
+          Colors.green[800],
+          Colors.green[300],
+          Colors.green[100]];
         _headingColor = Colors.white;
-        _loginYOffSet = 260;
+        _loginYOffSet = 230;
+        _titleXOffSet = -60;
+        _titleYOffSet = 70;
+        _iconXOffSet = 80;
+        _iconYOffSet = -10;
         break;
     }
 
     return Scaffold(
       body: Stack(children: <Widget>[
-        AnimatedContainer(
-          curve: Curves.fastLinearToSlowEaseIn,
-          duration: Duration(seconds: 1),
-          color: _backgroundColor,
-          child: Center(
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(50, 0, 50, 0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: <Widget>[
-                  Container(
-                    child: Container(
-                        child: Column(children: <Widget>[
-                      Text(
+        SafeArea(
+          child: AnimatedContainer(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: _backgroundColor,
+              )
+            ),
+            curve: Curves.fastLinearToSlowEaseIn,
+            duration: Duration(seconds: 1),
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(50, 0, 50, 0),
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(
+                      height: 30,
+                    ),
+                    AnimatedContainer(
+                      curve: Curves.fastLinearToSlowEaseIn,
+                      duration: Duration(seconds: 1),
+                      transform: Matrix4.translationValues(
+                          _titleXOffSet, _titleYOffSet, 1),
+                      child: Text(
                         'Return Med',
                         style: TextStyle(
                             color: _headingColor,
                             fontSize: 27,
                             fontWeight: FontWeight.bold),
                       ),
-                      SizedBox(
-                        height: 30,
-                      ),
-                      CircleAvatar(
-                        radius: 50,
-                        backgroundImage: AssetImage('assets/icon.png'),
-                      )
-                    ])),
-                  ),
-                  Container(
-                      child: Text(
-                    "INTRO HERE",
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                  )),
-                  Container(
-                    child: GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          if (_pageState == 1) {
-                            _pageState = 0;
-                          } else {
-                            _pageState = 1;
-                          }
-                        });
-                      },
-                      child: Container(
-                        margin: EdgeInsets.all(20),
-                        padding: EdgeInsets.all(20),
-                        width: double.infinity,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(30),
-                            color: Colors.green[600]),
-                        child: Center(
-                          child: Text(
-                            'Get started',
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    AnimatedContainer(
+                        curve: Curves.fastLinearToSlowEaseIn,
+                        duration: Duration(seconds: 1),
+                        transform: Matrix4.translationValues(
+                            _iconXOffSet, _iconYOffSet, 1),
+                        child: CircleAvatar(
+                          radius: 50,
+                          backgroundImage: AssetImage('assets/icon.png'),
+                        )),
+                    SizedBox(
+                      height: 200,
+                    ),
+                    Container(
+                        child: Text(
+                      "INTRO HERE",
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    )),
+                    SizedBox(
+                      height: 150,
+                    ),
+                    Container(
+                      child: GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            if (_pageState == 1) {
+                              _pageState = 0;
+                            } else {
+                              _pageState = 1;
+                            }
+                          });
+                        },
+                        child: Container(
+                          margin: EdgeInsets.all(20),
+                          padding: EdgeInsets.all(20),
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(30),
+                              color: Colors.green[600]),
+                          child: Center(
+                            child: Text(
+                              'Get started',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold),
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                  )
-                ],
+                    )
+                  ],
+                ),
               ),
             ),
           ),
         ),
         GestureDetector(
-          onTap: (){
+          onTap: () {
             setState(() {
               _pageState = 0;
             });
@@ -241,15 +339,18 @@ class _HomePageState extends State<HomePage> {
           child: AnimatedContainer(
             curve: Curves.fastLinearToSlowEaseIn,
             duration: Duration(seconds: 1),
-            transform:  Matrix4.translationValues(0, _loginYOffSet, 1),
+            transform: Matrix4.translationValues(0, _loginYOffSet, 1),
             decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30),
+                  topRight: Radius.circular(30),
+                )),
+            child: Container(
               color: Colors.white,
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(25),
-                topRight: Radius.circular(25),
-              )
+              margin: EdgeInsets.only(top: 30),
+              child: LoginPage(),
             ),
-            child: LoginPage(),
           ),
         )
       ]),
