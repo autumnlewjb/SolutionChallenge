d277d4e735b02a87cfd31c2d8179ce5c3eb7b981
unknown <lohyuenmin@gmail.com>
2021-02-08 14:36:06 +0800

    Add authentication feature

---
+++ android/app/src/main/kotlin/com/solutionchallenge/return_med/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.solutionchallenge.return_med
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/auth.dart
@@ -0,0 +1,117 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:flutter_facebook_login/flutter_facebook_login.dart';
+
+class Auth {
+  String error='';
+  final FirebaseAuth _firebaseAuth=FirebaseAuth.instance;
+  final FacebookLogin facebookSignIn=FacebookLogin();
+  final GoogleSignIn googleSignIn = GoogleSignIn(
+    scopes: <String>['email'],
+  );
+
+  //Create new account
+  Future<String> createUser(String email, String password) async {
+    try {
+      UserCredential credential = await _firebaseAuth
+          .createUserWithEmailAndPassword(email: email, password: password);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        error='Password should be at least 6 characters';
+      } else if (e.code == 'email-already-in-use') {
+        error='An account already exists for that email.';
+      }else if (e.code == 'invalid-email') {
+        error='Please check your email address and try again.';
+      }else if (e.code == 'unknown') {
+        error='Email and password cannot be blank';
+      }else{
+        error=e.toString();
+        print(e.toString());
+      }
+    }
+    return error;
+  }
+
+  //Sign in with email and password
+  Future<String> signIn(String email, String password) async {
+    try {
+      UserCredential credential = await _firebaseAuth
+          .signInWithEmailAndPassword(email: email, password: password);
+    } on FirebaseAuthException catch (e) {
+      if(e.code=='wrong-password'){
+        error='Incorrect password';
+      }else {
+        error='Invalid email or password';
+      }
+    }
+    return error;
+  }
+
+  //Sign out
+  Future<void> signOut() async {
+    _firebaseAuth.signOut();
+    print('Signed out');
+  }
+
+  //Sign in with Google account
+  Future<String> signInWithGoogle() async {
+    try {
+      final GoogleSignInAccount googleSignInAccount = await googleSignIn
+          .signIn();
+      final GoogleSignInAuthentication googleSignInAuthentication =
+      await googleSignInAccount.authentication;
+      final GoogleAuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleSignInAuthentication.accessToken,
+        idToken: googleSignInAuthentication.idToken,
+      );
+      await _firebaseAuth.signInWithCredential(credential);
+    }catch(e){
+      error=GoogleSignIn.kSignInFailedError;
+    }
+    return error;
+  }
+
+  //Google sign out
+  Future<void> signOutWithGoogle() async {
+    await googleSignIn.disconnect();
+    print('Signed out.');
+  }
+
+  //Sign in with Facebook account
+  Future<String> signInWithFacebook() async {
+    final FacebookLoginResult result = await facebookSignIn.logIn(['email']);
+    FacebookAuthCredential credential= FacebookAuthProvider.credential(result.accessToken.token);
+    await _firebaseAuth.signInWithCredential(credential);
+    switch (result.status) {
+      case FacebookLoginStatus.loggedIn:
+        final FacebookAccessToken accessToken = result.accessToken;
+        print('''
+         Logged in!
+
+         Token: ${accessToken.token}
+         User id: ${accessToken.userId}
+         Expires: ${accessToken.expires}
+         Permissions: ${accessToken.permissions}
+         Declined permissions: ${accessToken.declinedPermissions}
+         ''');
+        break;
+      case FacebookLoginStatus.cancelledByUser:
+        error='Login cancelled by the user.';
+        break;
+      case FacebookLoginStatus.error:
+        error='An error occurred: ${result.errorMessage}';
+        break;
+    }
+  }
+
+  //Facebook sign out
+  Future<void> signOutWithFacebook() async {
+    await facebookSignIn.logOut();
+    print('Signed out.');
+  }
+
+  Future<void> resetPassword(String email) async {
+    _firebaseAuth.sendPasswordResetEmail(email: email);
+  }
+
+}
