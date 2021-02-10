2d251b574b49a3f8fae55859383591612c2efeb3
J Sen Ooi <u2005254@siswa.um.edu.my>
2021-02-10 12:19:32 +0800

    added validation for date and scrolling capability

--- lib/Dashboard/Pages/schedule_return.dart
+++ lib/Dashboard/Pages/schedule_return.dart
@@ -47,6 +47,7 @@ class _ScheduleReturnState extends State<ScheduleReturn> {
       });
   }
 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -60,155 +61,167 @@ class _ScheduleReturnState extends State<ScheduleReturn> {
           child: Form(
             autovalidateMode: AutovalidateMode.onUserInteraction,
             key: _formKey,
-            child: Column(
-              children: [
-                Text(
-                  'Schedule a return',
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                TextFormField(
-                  validator: (val) => val.isEmpty ? 'Field required' : null,
-                  decoration: InputDecoration(
-                    labelText: 'Medicine Name',
-                    labelStyle: TextStyle(
-                      fontSize: 13.0,
-                      color: Colors.black,
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  SizedBox(height: 20.0,),
+                  Text(
+                    'Schedule a return',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
                     ),
-                    hintText: 'Medicine Name',
-                    hintStyle: TextStyle(
-                      fontSize: 10.0,
-                    ),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(15.0)),
                   ),
-                  onChanged: (val) {
-                    setState(() {
-                      medName = val;
-                    });
-                  },
-                ),
-                GestureDetector(
-                  onTap: () => _selectDate(context),
-                  child: AbsorbPointer(
-                    child: TextField(
-                      decoration: InputDecoration(
-                        labelText: '$date',
-                        labelStyle:
-                            TextStyle(fontSize: 13.0, color: Colors.black),
-                        icon: Icon(
-                          Icons.calendar_today,
+                  SizedBox(height: 20.0,),
+                  TextFormField(
+                    validator: (val) => val.isEmpty ? 'Field required' : null,
+                    decoration: InputDecoration(
+                      labelText: 'Medicine Name',
+                      labelStyle: TextStyle(
+                        fontSize: 13.0,
+                        color: Colors.black,
+                      ),
+                      hintText: 'Medicine Name',
+                      hintStyle: TextStyle(
+                        fontSize: 10.0,
+                      ),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15.0)),
+                    ),
+                    onChanged: (val) {
+                      setState(() {
+                        medName = val;
+                      });
+                    },
+                  ),
+                  SizedBox(height: 10.0,),
+                  GestureDetector(
+                    onTap: () => _selectDate(context),
+                    child: AbsorbPointer(
+                      child: TextFormField(
+                        validator: (val) => date=="Medicine Expiry Date (yyyy-mm-dd)"? 'Field required' : null,
+                        decoration: InputDecoration(
+                          labelText: '$date',
+                          labelStyle:
+                          TextStyle(fontSize: 13.0, color: Colors.black),
+                          icon: Icon(
+                            Icons.calendar_today,
+                          ),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15.0)),
                         ),
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(15.0)),
+                        keyboardType: null,
                       ),
                     ),
                   ),
-                ),
-                TextFormField(
-                  validator: (val) => val.isEmpty ? 'Field required' : null,
-                  decoration: InputDecoration(
-                    labelText: 'Address Line 1',
-                    labelStyle: TextStyle(
-                      fontSize: 13.0,
-                      color: Colors.black,
+                  SizedBox(height: 10.0,),
+                  TextFormField(
+                    validator: (val) => val.isEmpty ? 'Field required' : null,
+                    decoration: InputDecoration(
+                      labelText: 'Address Line 1',
+                      labelStyle: TextStyle(
+                        fontSize: 13.0,
+                        color: Colors.black,
+                      ),
+                      hintText: 'taman admin',
+                      hintStyle: TextStyle(
+                        fontSize: 10.0,
+                      ),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15.0)),
                     ),
-                    hintText: 'taman admin',
-                    hintStyle: TextStyle(
-                      fontSize: 10.0,
-                    ),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(15.0)),
-                  ),
-                  onChanged: (val) {
-                    setState(() {
-                      address1 = val;
-                    });
-                  },
-                ),
-                TextFormField(
-                  validator: (val) => val.isEmpty ? 'Field required' : null,
-                  decoration: InputDecoration(
-                    labelText: 'Address Line 2',
-                    labelStyle: TextStyle(
-                      fontSize: 13.0,
-                      color: Colors.black,
-                    ),
-                    hintText: 'jalan admin',
-                    hintStyle: TextStyle(
-                      fontSize: 10.0,
-                    ),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(15.0)),
-                  ),
-                  onChanged: (val) {
-                    setState(() {
-                      address2 = val;
-                    });
-                  },
-                ),
-                Container(
-                    height: 50,
-                    padding: EdgeInsets.only(left: 16, right: 16),
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.grey, width: 1),
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    child: Center(
-                      child: DropdownButtonFormField(
-                          decoration: InputDecoration(
-                            errorStyle: TextStyle(height: 0, fontSize: 0),
-                          ),
-                          validator: (value) => value == null ? '' : null,
-                          isExpanded: true,
-                          hint: Text('Choose state'),
-                          value: state,
-                          onChanged: (newState) {
-                            setState(() {
-                              state = newState;
-                            });
-                          },
-                          items: states.map((valueItems) {
-                            return DropdownMenuItem(
-                              value: valueItems,
-                              child: Text(valueItems),
-                            );
-                          }).toList()),
-                    )),
-                TextFormField(
-                  validator: (val) => val.isEmpty ? 'Field required' : null,
-                  decoration: InputDecoration(
-                    labelText: 'Post Code',
-                    labelStyle: TextStyle(
-                      fontSize: 13.0,
-                      color: Colors.black,
-                    ),
-                    hintText: '05400',
-                    hintStyle: TextStyle(
-                      fontSize: 10.0,
-                    ),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(15.0)),
-                  ),
-                  onChanged: (val) {
-                    setState(() {
-                      postcode = val;
-                    });
-                  },
-                ),
-                ElevatedButton(
-                    onPressed: () {
-                      if (_formKey.currentState.validate()) {
-                        ReturnInfo(medName, selectedDate, address1, address2,
-                            state, postcode);
-                      }
+                    onChanged: (val) {
+                      setState(() {
+                        address1 = val;
+                      });
                     },
-                    child: Icon(
-                      Icons.assignment_turned_in,
-                    ))
-              ],
+                  ),
+                  SizedBox(height: 10.0,),
+                  TextFormField(
+                    validator: (val) => val.isEmpty ? 'Field required' : null,
+                    decoration: InputDecoration(
+                      labelText: 'Address Line 2',
+                      labelStyle: TextStyle(
+                        fontSize: 13.0,
+                        color: Colors.black,
+                      ),
+                      hintText: 'jalan admin',
+                      hintStyle: TextStyle(
+                        fontSize: 10.0,
+                      ),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15.0)),
+                    ),
+                    onChanged: (val) {
+                      setState(() {
+                        address2 = val;
+                      });
+                    },
+                  ),
+                  SizedBox(height: 10.0,),
+                  Container(
+                      height: 50,
+                      padding: EdgeInsets.only(left: 16, right: 16),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.grey, width: 1),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Center(
+                        child: DropdownButtonFormField(
+                            decoration: InputDecoration(
+                              errorStyle: TextStyle(height: 0, fontSize: 0),
+                            ),
+                            validator: (value) => value == null ? '' : null,
+                            isExpanded: true,
+                            hint: Text('Choose state'),
+                            value: state,
+                            onChanged: (newState) {
+                              setState(() {
+                                state = newState;
+                              });
+                            },
+                            items: states.map((valueItems) {
+                              return DropdownMenuItem(
+                                value: valueItems,
+                                child: Text(valueItems),
+                              );
+                            }).toList()),
+                      )),
+                  SizedBox(height: 10.0,),
+                  TextFormField(
+                    validator: (val) => val.isEmpty ? 'Field required' : null,
+                    decoration: InputDecoration(
+                      labelText: 'Post Code',
+                      labelStyle: TextStyle(
+                        fontSize: 13.0,
+                        color: Colors.black,
+                      ),
+                      hintText: '05400',
+                      hintStyle: TextStyle(
+                        fontSize: 10.0,
+                      ),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15.0)),
+                    ),
+                    onChanged: (val) {
+                      setState(() {
+                        postcode = val;
+                      });
+                    },
+                  ),
+                  SizedBox(height: 10.0,),
+                  ElevatedButton(
+                      onPressed: () {
+                        if (_formKey.currentState.validate()) {
+                          //ReturnInfo(medName, selectedDate, address1, address2,
+                          //    state, postcode);
+                        }
+                      },
+                      child: Icon(
+                        Icons.assignment_turned_in,
+                      ))
+                ],
+              ),
             ),
           ),
         ));
