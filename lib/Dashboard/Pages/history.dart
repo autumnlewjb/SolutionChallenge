013372b650f87c96714f186b5416db864050cead
Chia Wei Jie <chiaweijie_01@hotmail.com>
2021-02-21 14:29:59 +0800

    update history design

--- lib/Dashboard/Pages/history.dart
+++ lib/Dashboard/Pages/history.dart
@@ -13,31 +13,44 @@ class _HistoryState extends State<History> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('On going history'),
+        elevation: 0,
+        centerTitle: true,
+        title: Text('Ongoing history'),
         backgroundColor: Colors.deepPurple,
       ),
-      body: Center(
-        child: Consumer<List<ReturnInfo>>(builder: (_, returnList, __) {
-          if (returnList == null) {
-            return CircularProgressIndicator();
-          }
-          returnList = returnList
-              .where((element) => element.status == 'Completed')
-              .toList();
-          if (returnList.isEmpty) {
-            return Text(
-              'No ongoing return found. Feel free to schedule a return now! :)',
-              style:
-                  TextStyle(fontWeight: FontWeight.bold, color: Colors.black),
-            );
-          }
-          return ListView.builder(
-              itemCount: returnList.length,
-              itemBuilder: (context, index) {
-                ReturnInfo info = returnList[index];
-                return WidgetModel().schReturnTile(this.context, context, info);
-              });
-        }),
+      body: Container(
+        padding: EdgeInsets.fromLTRB(10, 20, 10, 0),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(colors: [
+            Colors.deepPurple,
+            Colors.deepPurple[400],
+            Colors.deepPurple[300],
+            Colors.deepPurple[200]
+          ], begin: Alignment.topCenter, end: Alignment.bottomCenter),
+        ),
+        child: Center(
+          child: Consumer<List<ReturnInfo>>(builder: (_, returnList, __) {
+            if (returnList == null) {
+              return CircularProgressIndicator();
+            }
+            returnList = returnList
+                .where((element) => element.status == 'Completed')
+                .toList();
+            if (returnList.isEmpty) {
+              return Text(
+                'No ongoing return found. Feel free to schedule a return now! :)',
+                style:
+                    TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+              );
+            }
+            return ListView.builder(
+                itemCount: returnList.length,
+                itemBuilder: (context, index) {
+                  ReturnInfo info = returnList[index];
+                  return WidgetModel().schReturnTile(this.context, context, info);
+                });
+          }),
+        ),
       ),
     );
   }
