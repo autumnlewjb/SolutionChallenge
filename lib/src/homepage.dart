3e5b651c696db317c18c995a63f8e586ea9264ff
JBLew <autumnlewjb@gmail.com>
2021-02-26 17:55:14 +0800

    login page with better ux

--- lib/src/homepage.dart
+++ lib/src/homepage.dart
@@ -13,39 +13,27 @@ class _HomePageState extends State<HomePage> {
     Colors.deepPurple[600]
   ];
   var _headingColor = Colors.black;
-  double _loginYOffSet = 0;
 
-  double _titleXOffSet = 0;
-  double _titleYOffSet = 0;
-
-  double _iconXOffSet = 0;
-  double _iconYOffSet = 0;
-
-  double _windowWidth = 0;
   double _windowHeight = 0;
+  double _loginYOffSet = 0;
   int _pageState = 0;
+
+  final message = "Return you meds.\nSave the environment.";
 
   @override
   Widget build(BuildContext context) {
     _windowHeight = MediaQuery.of(context).size.height;
-    _windowWidth = MediaQuery.of(context).size.width;
-
     var _animatedWidget;
-
     switch (_pageState) {
       case 0:
         _loginYOffSet = _windowHeight;
-        _titleXOffSet = 0;
-        _titleYOffSet = 0;
-        _iconXOffSet = 0;
-        _iconYOffSet = 0;
         _backgroundColor = [
           Colors.deepPurple[300],
           Colors.deepPurple,
           Colors.deepPurple[600]
         ];
         _headingColor = Colors.white;
-        _animatedWidget = _columnFormat();
+        _animatedWidget = _titleInColumn();
         break;
       case 1:
         _backgroundColor = [
@@ -55,19 +43,12 @@ class _HomePageState extends State<HomePage> {
         ];
         _headingColor = Colors.white;
         _loginYOffSet = _windowHeight * 3 / 10;
-        _titleXOffSet = -_windowWidth * 1.5 / 10;
-        _titleYOffSet = _windowHeight * 0.7 / 10;
-        _iconXOffSet = _windowWidth * 2.25 / 10;
-        _iconYOffSet = -_windowHeight * 0.4 / 10;
-        _animatedWidget = _rowFormat();
+        _animatedWidget = _titleInRow();
         break;
       case 2:
         _headingColor = Colors.white;
         _loginYOffSet = 0;
-        _titleXOffSet = 0;
-        _titleYOffSet = 0;
-        _iconXOffSet = 0;
-        _iconYOffSet = 0;
+        _animatedWidget = _titleInColumn();
         break;
     }
 
@@ -75,6 +56,7 @@ class _HomePageState extends State<HomePage> {
       resizeToAvoidBottomInset: false,
       body: Stack(children: <Widget>[
         Container(
+          padding: EdgeInsets.fromLTRB(20, 0, 20, 30),
           decoration: BoxDecoration(
               gradient: LinearGradient(
             begin: Alignment.topLeft,
@@ -82,81 +64,66 @@ class _HomePageState extends State<HomePage> {
             colors: _backgroundColor,
           )),
           child: SafeArea(
-            child: AnimatedContainer(
-              curve: Curves.fastLinearToSlowEaseIn,
-              duration: Duration(seconds: 1),
-              child: Center(
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(50, 0, 50, 0),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      Expanded(
-                        flex: 3,
-                        child: Container(
-                          alignment: Alignment.center,
-                          child: AnimatedSwitcher(
-                              switchInCurve: Curves.easeInToLinear,
-                              switchOutCurve: Curves.easeInToLinear,
-                              duration: Duration(milliseconds: 500),
-                              transitionBuilder:
-                                  (Widget child, Animation<double> animation) =>
-                                      ScaleTransition(
-                                          child: child, scale: animation),
-                              child: _animatedWidget),
+            child: Center(
+              child: Container(
+                child: Column(
+                  children: <Widget>[
+                    Expanded(
+                      flex: 3,
+                      child: AnimatedSwitcher(
+                          switchInCurve: Curves.easeInToLinear,
+                          switchOutCurve: Curves.easeInToLinear,
+                          duration: Duration(milliseconds: 500),
+                          child: _animatedWidget),
+                    ),
+                    Expanded(
+                      flex: 4,
+                      child: Container(
+                        child: Text(
+                          message,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
                         ),
                       ),
-                      Expanded(
-                        flex: 4,
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            if (_pageState == 1) {
+                              _pageState = 0;
+                            } else {
+                              _pageState = 1;
+                            }
+                          });
+                        },
                         child: Container(
-                          child: Text(
-                            "INTRO HERE",
-                            style: TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.white),
-                          ),
-                        ),
-                      ),
-                      Expanded(
-                        flex: 1,
-                        child: Container(
-                          child: GestureDetector(
-                            onTap: () {
-                              setState(() {
-                                if (_pageState == 1) {
-                                  _pageState = 0;
-                                } else {
-                                  _pageState = 1;
-                                }
-                              });
-                            },
-                            child: Container(
-                              decoration: BoxDecoration(
-                                  boxShadow: [
-                                    BoxShadow(
-                                        offset: Offset(0, 5),
-                                        blurRadius: 50,
-                                        color: Colors.black.withOpacity(0.4))
-                                  ],
-                                  borderRadius: BorderRadius.circular(30),
-                                  color: Colors.white),
-                              child: Center(
-                                child: Text(
-                                  'Get started',
-                                  style: TextStyle(
-                                      color: Colors.deepPurple,
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                              ),
+                          decoration: BoxDecoration(
+                              boxShadow: [
+                                BoxShadow(
+                                    offset: Offset(0, 5),
+                                    blurRadius: 50,
+                                    color: Colors.black.withOpacity(0.4))
+                              ],
+                              borderRadius: BorderRadius.circular(30),
+                              color: Colors.white),
+                          child: Center(
+                            child: Text(
+                              'Get started',
+                              style: TextStyle(
+                                  color: Colors.deepPurple,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold),
                             ),
                           ),
                         ),
                       ),
-                      Expanded(flex: 1, child: Container()),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
             ),
@@ -177,82 +144,92 @@ class _HomePageState extends State<HomePage> {
               });
             },
             onVerticalDragUpdate: (details) {
-              if (details.delta.dy < 0 && _loginYOffSet > 0) {
-                setState(() {
-                  _pageState = 2;
-                  _loginYOffSet = 0;
-                });
-              } else if (details.delta.dy > 0 && _loginYOffSet == 0) {
+              if (_pageState == 1) {
+                if (details.delta.dy < 0) {
+                  setState(() {
+                    _pageState = 2;
+                  });
+                } else if (details.delta.dy > 30) {
+                  setState(() {
+                    _pageState = 0;
+                  });
+                }
+              } else if (details.delta.dy > 0 && _pageState == 2) {
                 setState(() {
                   _pageState = 1;
                 });
               }
             },
-            child: AnimatedContainer(
-              curve: Curves.fastLinearToSlowEaseIn,
-              duration: Duration(seconds: 1),
-              // padding: EdgeInsets.only(top: 20.0),
-              transform: Matrix4.translationValues(0, _loginYOffSet, 1),
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(30),
-                    topRight: Radius.circular(30),
-                  )),
-              child: Column(
-                children: [
-                  Container(
-                    padding: EdgeInsets.only(top: 10.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(Icons.swap_vert_circle_rounded),
-                        Container(
-                          padding: EdgeInsets.only(left: 5),
-                          child: Text(
-                            _pageState == 1
-                                ? "Swipe Up for Full View"
-                                : "Swipe Down for Better View",
-                            style: TextStyle(color: Colors.grey),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Expanded(
-                    child: SafeArea(
-                      child: LoginPage(),
-                    ),
-                  ),
-                ],
-              ),
-            ),
+            child: _loginSheet(),
           ),
         ),
       ]),
     );
   }
 
-  Widget _columnFormat() {
+  Widget _loginSheet() {
+    return AnimatedContainer(
+      curve: Curves.fastLinearToSlowEaseIn,
+      duration: Duration(seconds: 1),
+      transform: Matrix4.translationValues(0, _loginYOffSet, 1),
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(30),
+            topRight: Radius.circular(30),
+          )),
+      child: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.only(top: 10.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.swap_vert_circle_rounded),
+                Container(
+                  padding: EdgeInsets.only(left: 5),
+                  child: Text(
+                    _pageState == 1
+                        ? "Swipe Up for Full View"
+                        : "Swipe Down for Better View",
+                    style: TextStyle(color: Colors.grey),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: SafeArea(
+              child: LoginPage(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _titleInColumn() {
     return Column(
       mainAxisAlignment: MainAxisAlignment.start,
       children: [
-        Container(
-          padding: EdgeInsets.fromLTRB(0, 30, 0, 0),
-          // transform: Matrix4.translationValues(_titleXOffSet, _titleYOffSet, 1),
-          child: Text(
-            'Return Med',
-            style: TextStyle(
-                color: _headingColor,
-                fontSize: 27,
-                fontWeight: FontWeight.bold),
+        Expanded(
+          flex: 1,
+          child: Container(
+            padding: EdgeInsets.fromLTRB(0, 30, 0, 0),
+            child: Text(
+              'Return Med',
+              style: TextStyle(
+                  color: _headingColor,
+                  fontSize: 27,
+                  fontWeight: FontWeight.bold),
+            ),
           ),
         ),
         Expanded(
+          flex: 2,
           child: Container(
             alignment: Alignment.topCenter,
             padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
-            // transform: Matrix4.translationValues(_iconXOffSet, _iconYOffSet, 1),
             child: CircleAvatar(
               radius: 50,
               backgroundImage: AssetImage('assets/icon.png'),
@@ -263,13 +240,14 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  Widget _rowFormat() {
+  Widget _titleInRow() {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
         Expanded(
           child: Container(
+            alignment: Alignment.center,
             child: Text(
               'Return Med',
               style: TextStyle(
@@ -281,7 +259,8 @@ class _HomePageState extends State<HomePage> {
         ),
         Expanded(
           child: Container(
-            padding: EdgeInsets.all(20),
+            margin: EdgeInsets.all(0),
+            padding: EdgeInsets.all(5),
             child: CircleAvatar(
               radius: 50,
               backgroundImage: AssetImage('assets/icon.png'),
