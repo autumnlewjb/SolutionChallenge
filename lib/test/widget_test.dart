0e6d92188e21108ca9fcd32a1459c650b0c25efa
JBLew <autumnlewjb@gmail.com>
2021-02-27 22:09:57 +0800

    write more widget test

--- lib/test/widget_test.dart
+++ lib/test/widget_test.dart
@@ -20,10 +20,8 @@ void main() {
             create: (_) => Auth(auth), child: MaterialApp(home: widget)));
   }
 
-  group('Login', () {
-    testWidgets(
-        'Login form show correctly when user pressed on the get started button',
-        (WidgetTester tester) async {
+  group('Login form', () {
+    _getToLogin(WidgetTester tester) async {
       await tester.pumpWidget(buildTestableWidget(HomePage()));
 
       final button = find.text("Get started");
@@ -31,11 +29,61 @@ void main() {
       expect(find.text('Return Med'), findsOneWidget);
       expect(button, findsOneWidget);
 
-      await tester.tap(button.first);
+      await tester.tap(button);
       await tester.pump();
+    }
+
+    testWidgets('show correctly when user pressed on the get started button',
+        (WidgetTester tester) async {
+      await _getToLogin(tester);
 
       expect(find.text("Login").first, findsOneWidget);
-      expect(find.byType(ElevatedButton).first, findsOneWidget);
+      expect(find.byType(ElevatedButton), findsOneWidget);
+    });
+
+    testWidgets("Swiping works right", (WidgetTester tester) async {
+      await _getToLogin(tester);
+
+      await tester.pump();
+      expect(find.text("Swipe Up for Full View"), findsOneWidget);
+      expect(find.text("Swipe Down for Better View"), findsNothing);
+
+      await tester.pumpAndSettle();
+      await tester.drag(find.text("Swipe Up for Full View"), Offset(0, -10.0));
+      await tester.pumpAndSettle();
+
+      expect(find.text("Swipe Up for Full View"), findsNothing);
+      expect(find.text("Swipe Down for Better View"), findsOneWidget);
+    });
+
+    testWidgets("show appropriate validation message with empty textfield",
+        (WidgetTester tester) async {
+      await _getToLogin(tester);
+
+      await tester.pumpAndSettle();
+      await tester.tap(find.byType(ElevatedButton));
+      await tester.pump(const Duration(milliseconds: 500));
+
+      expect(find.text("Enter an email"), findsOneWidget);
+      expect(find.text("Enter a password"), findsOneWidget);
+
+      await tester.enterText(find.byKey(ValueKey("password-field")), "123456");
+      await tester.pumpAndSettle();
+      await tester.tap(find.byType(ElevatedButton));
+      await tester.pump(const Duration(milliseconds: 500));
+
+      expect(find.text("Enter a password"), findsNothing);
+      expect(find.text("Enter an email"), findsOneWidget);
+
+      await tester.enterText(find.byKey(ValueKey("password-field")), "");
+      await tester.enterText(
+          find.byKey(ValueKey("email-field")), "test@returnmeds.com");
+      await tester.pumpAndSettle();
+      await tester.tap(find.byType(ElevatedButton));
+      await tester.pump(const Duration(milliseconds: 500));
+
+      expect(find.text("Enter a password"), findsOneWidget);
+      expect(find.text("Enter an email"), findsNothing);
     });
   });
 }
